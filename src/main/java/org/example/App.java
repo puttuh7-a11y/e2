d0afa9123df8839e626d24e8ce46a8ceb8b7ea4b
Puttraj Halli <puttuh7@gmail.com>
2026-06-26 12:11:46 +0530

    app file are added for website

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,12 @@
 package org.example;
-
-public class App {
-    public int add(int a, int b){
-        return a + b;
+public class App{
+    public int add(int a , int b){
+        return a+b;
     }
     public static void main(String[] args){
-        App app = new App();
-        int result = app.add(4,2);
-        System.out.println("4+ 2 = " + result);
-        System.out.println("exe  cuted  apllication successfully:");
+        App app=new App();
+        int result = app.add( 4,2);
+        System.out.println("4+2="+result);
+        System.out.println("excuted application sucessfuly");
     }
 }
